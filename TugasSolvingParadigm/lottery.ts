d770ac88b1acc6d63cf0e1d72665dcf8ecc775e5
FransGumaran <fransgumaran@gmail.com>
2023-07-18 21:28:00 +0700

    Upload tugas solving paradigm

---
+++ TugasSolvingParadigm/lottery.ts
@@ -0,0 +1,23 @@
+function lottery(num: number): Promise<string> {
+    
+    return new Promise((resolve, reject) => {
+      console.log("Undian lotre dimulai...");
+      console.log("Sedang mengundi nomor Anda...");
+  
+      setTimeout(() => {
+        const randomNumber = Math.floor(Math.random() * 1000) + 1;
+  
+        if (num === randomNumber) {
+          resolve("Selamat, Anda mendapatkan hadiah utama berupa mobil!");
+        } else {
+          reject("Maaf, Anda kurang beruntung.");
+        }
+      }, 10000);
+    });
+  }
+  
+  
+  lottery(5)
+    .then((res) => console.log(res))
+    .catch((err) => console.log(err))
+    .finally(() => console.log("Undian lotre telah berakhir..."));
